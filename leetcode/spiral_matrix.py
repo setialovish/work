05c81429eb8de18652761b3eb6a886b377501fb2
Lovish Setia <setial@lg12-24.scss.tcd.ie>
2019-01-10 18:35:33 +0000

    better version of spiral matrix

--- leetcode/spiral_matrix.py
+++ leetcode/spiral_matrix.py
@@ -4,108 +4,28 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        #Global variables
-        list_output = []
-        str_change_var = 'j'
-        str_inc_dec = 'inc'
-        int_i = 0
-        int_j = 0
-        int_change_counter = 0
-
         
-        if len(matrix) == 0 or len(matrix[0]) == 0 :
+        if len(matrix) == 0:
             return []
-        elif len(matrix) == 1:
-            return matrix[0]
+        list_output = []
+        list_temp = []
         int_m = len(matrix)
         int_n = len(matrix[0])
-
         
         
-        def change_direction():
-            nonlocal int_change_counter, str_change_var,str_inc_dec
+        while True:
             
-            int_change_counter += 1
-            
-            '''increment and decrement shall take place like
-            inc, inc, dec, dec, inc, inc, dec, dec'''    
-            if (int_change_counter % 2) == 0 and int_change_counter != 0:
+            for element in matrix[0]:
+                list_output.append(element)
                 
-                if str_inc_dec == 'inc':
-                    str_inc_dec = 'dec'
-                else:
-                    str_inc_dec = 'inc'
-                                        
+            del matrix[0]
             
-            if str_change_var == 'j':
-                str_change_var = 'i'
-            elif str_change_var == 'i':
-                str_change_var = 'j'
-            
-            
-        def increment_decrement():
-        
-            nonlocal  int_i, int_j
-            
-
-            
-            if str_inc_dec == 'inc':
-                if str_change_var =='j':
-                    int_j += 1
-                else:
-                    int_i += 1
-            elif str_inc_dec == 'dec':
-                if str_change_var == 'j':
-                    int_j -= 1
-                else:
-                    int_i -= 1
-            
-                
-        def revert():
-            nonlocal int_i, int_j
-            if str_inc_dec == 'dec':
-                if str_change_var == 'i':
-                    int_i += 1
-                else:
-                    int_j += 1
-            else:
-                if str_change_var == 'i':
-                    int_i -= 1
-                else:
-                    int_j -= 1
-            
-            
-        #for i in range(len(matrix)**2+3):
-        while True:    
-            ''' 4 conditions to be checked
-            1) i==0 and j==len(matrix) - 1
-            2) i==len(matrix)-1 and j==len(matrix)-1
-            3) i==len(matrix)-1 and j==0
-            4) matrix[i][j] already in the o/p matrix'''
-
-            
-            if int_i==0 and int_j== int_n-1:
-                list_output.append(matrix[int_i][int_j])
-                change_direction()
-            elif int_i == int_m-1 and int_j==int_n-1:
-                list_output.append(matrix[int_i][int_j])
-                change_direction()                
-            elif int_i==int_m-1 and int_j==0:
-                list_output.append(matrix[int_i][int_j])
-                change_direction()
-            elif matrix[int_i][int_j] in list_output:
-                
-                #come to the previous location somehow 
-                revert()
-                change_direction()
-                
-            else:
-                list_output.append(matrix[int_i][int_j])
-                
-                #increment_decrement()
-            increment_decrement()
-            if len(list_output) == int_m*int_n:
+            if len(matrix) == 0:
+                return list_output
                 break
+            
+            for i in range(len(matrix[0])-1,-1,-1):
+                list_temp.append(list(item[i] for item in matrix))
+            matrix=list_temp
+            list_temp = []
         
-        
-        return list_output
